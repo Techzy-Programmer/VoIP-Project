7be202919513afb8a7bdaea570cbdef8b8569d00
Rishabh Kumar <techzy.programmer@gmail.com>
2021-12-01 18:07:03 +0530

    Added JSON references & removed embedded modules

--- BoltCall/Program.cs
+++ BoltCall/Program.cs
@@ -17,8 +17,6 @@ namespace BoltCall
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            //if (Helper.Wire.SetupModules(out string[] Modules))
-            //    foreach (var SDll in Modules) Assembly.LoadFrom(SDll);
             Application.Run(new Interactives.Test());
         }
     }
